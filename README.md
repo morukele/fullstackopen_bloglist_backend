c0953c69bf8ff222922346ffb2a9c79e1e3b86cd
Oghenemarho Orukele <morukele@yahoo.com>
2021-11-01 22:51:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blog List Backend
+
+This repository contains the node backend for the bloglist backend of the fullstackopen 2021 course.
---
+++ models/blog.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const blogSchema = new mongoose.Schema({
+  title: { type: String, required: true, minlength: 5, unique: true },
+  author: { type: String, required: true, minlength: 5 },
+  url: { type: String, required: true, minlength: 5 },
+  likes: { type: Number, required: true, minlength: 5 },
+});
+
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+blogSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("Blog", blogSchema);
